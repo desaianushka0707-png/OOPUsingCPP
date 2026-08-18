5ca8b7c13dadc680ac532d401aa2990b5420a271
Anushka Chandrashekhar Desai <desaianushka0707@gmail.com>
2026-08-18 11:46:06 +0530

    Add files via upload

---
+++ studentData.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	public:
+		int rn;
+		string nm;
+		string mobNo;
+		
+		void accept()
+		{
+			cout<<"\n Enter student roll no:";
+			cin>>rn;
+			
+			cout<<" \n Enter student name:";
+			cin>>nm;
+			
+			cout<<" \n Enter student mobile no:";
+			cin>>mobNo;
+		}
+		
+		
+		void dispaly()
+		
+		{
+			cout<<"\n----Display the details----"<<endl;
+			cout<<" \n Roll No:"<<rn<<endl;
+			cout<<" Name:"<<nm<<endl;
+			cout<<" Mobile No:"<<mobNo<<endl;
+		}
+};
+int main()
+{
+	student s1;
+	s1.accept();
+	s1.dispaly();
+	
+	return 0;
+}
